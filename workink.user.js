3b704b6e64f54a69bf25a16d6055608075b86c57
Difz25x <difz25x@gmail.com>
2025-11-06 05:45:59 +0700

    Update workink.user.js

--- workink.user.js
+++ workink.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Volcano & Work.ink Bypass
 // @namespace    http://tampermonkey.net/
-// @version      1.0.6.0
+// @version      1.0.6.1
 // @description  Bypass Volcano and Work.ink links with unified panel
 // @author       Nadhif
 // @match        https://key.volcano.wtf/*
